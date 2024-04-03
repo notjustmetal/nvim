b02826a31bbd11290efe20f1a79ff381e79d84ca
Abrar Bhuiyan <53502@icf.com>
2024-04-03 11:09:40 -0400

    feat: add tab completion

--- lua/notjustmetal/plugins/nvim-cmp.lua
+++ lua/notjustmetal/plugins/nvim-cmp.lua
@@ -4,16 +4,27 @@ return {
 
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
   },
 
   config = function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
+      },
+
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end
       },
 
       mapping = cmp.mapping.preset.insert({
@@ -24,13 +35,45 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true))
+          else
+            fallback()
+          end
+        end,
+
+        ["<S-Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.expand_or_jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true))
+          else
+            fallback()
+          end
+        end
       }),
 
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" }
+        { name = "nvim_lua" }
       })
+    })
+
+    cmp.setup.cmdline("/", {
+      sources = {
+        { name = "buffer" }
+      }
+    })
+
+    cmp.setup.cmdline(":", {
+      sources = {
+        { name = "path" },
+        { name = "cmdline" }
+      }
     })
   end
 }
